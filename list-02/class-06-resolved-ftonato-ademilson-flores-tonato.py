ee75b0c410ae2c624a1f42c010bcebc337345556
Gabriel Becker <gabrieloliveirabecker@gmail.com>
2015-12-05 00:20:08 -0200

    Add 'encoding: UTF-8' for to work with python 2.7 (xubuntu 12.04)

--- list-02/class-06-resolved-ftonato-ademilson-flores-tonato.py
+++ list-02/class-06-resolved-ftonato-ademilson-flores-tonato.py
@@ -1,3 +1,5 @@
+# -*- coding: UTF-8 -*-
+
 # 6) Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
 # Calcule e mostre o total do seu salário no referido mês, sabendo-se que são descontados 11% para o Imposto de Renda, 8% para o INSS e 5% para o sindicato, faça um programa que nos dê o salário bruto, quanto pagou ao INSS, quanto pagou ao sindicato e o salário líquido.
 # Observe que Salário Bruto - Descontos = Salário Líquido. Calcule os descontos e o salário líquido, conforme a tabela abaixo:
